2f5fcfcacfb2fe7a2d40bf7b93ce9d2ddf505547
RicardoFaria <ricardofcosta0804@gmail.com>
2022-03-08 18:47:56 -0300

    Perceptron.v0.1

---
+++ perceptron-1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 16:56:10 2022
+
+@author: ricar
+"""
+
+entradas = [-1, 7, 5]
+pesos = [0.8, 0.1, 0]
+
+
+def soma(e, p):
+    s = 0
+    for i in range(entradas.len+1):
+        #print(entradas[i])
+        #print(pesos[i])
+        s += e[i] * p[i]
+    return s
+        
+
+def stepFunc(soma):
+    if soma >= 1:
+        return 1
+    return 0
+
+s = soma(entradas, pesos)
+
+result = stepFunc(s)
+
